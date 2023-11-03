0c5df4cbf36b545a7bcfec5d822a7b6748ccd2d6
alex <766f6964+github@gmail.com>
2023-11-03 12:13:34 +0100

    fixed quotes & release date field type

--- src/Schema/PDOMariaDBSchema.php
+++ src/Schema/PDOMariaDBSchema.php
@@ -8,20 +8,20 @@ final class PDOMariaDBSchema implements InterfaceSchema
 
     private const INSTALL_QUERIES = array(
         '
-                CREATE TABLE "VERSION" (
-                    "release_number" INTEGER NOT NULL,
-                    "release_date" STRING NOT NULL,
-                    PRIMARY KEY("release_number")
-                );
+            CREATE TABLE `VERSION` (
+                `release_number` INTEGER NOT NULL,
+                `release_date` CHAR(19) NOT NULL,
+                PRIMARY KEY(`release_number`)
+            );
         ',
         '
-                INSERT INTO "VERSION" (release_number, release_date) VALUES (0, CURRENT_TIMESTAMP);
+            INSERT INTO `VERSION` (release_number, release_date) VALUES (0, CURRENT_TIMESTAMP);
         '
     );
 
-    private const SET_CURRENT_VERSION_QUERY = ' INSERT INTO "VERSION" (release_number, release_date) VALUES (:release_number, CURRENT_TIMESTAMP); ';
+    private const SET_CURRENT_VERSION_QUERY = ' INSERT INTO `VERSION` (release_number, release_date) VALUES (:release_number, CURRENT_TIMESTAMP); ';
 
-    private const GET_CURRENT_VERSION_QUERY = ' SELECT release_number FROM "VERSION" ORDER BY release_number DESC LIMIT 1; ';
+    private const GET_CURRENT_VERSION_QUERY = ' SELECT release_number FROM `VERSION` ORDER BY release_number DESC LIMIT 1; ';
 
     public function __construct(string $upgradeSchemaPath = "")
     {
