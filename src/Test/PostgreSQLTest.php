8980a1af46508e00605d98ea5b16122b4c251332
alex <766f6964+github@gmail.com>
2025-11-09 21:53:05 +0100

    warnings fixed (php-stan)

--- src/Test/PostgreSQLTest.php
+++ src/Test/PostgreSQLTest.php
@@ -9,7 +9,7 @@ require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "vendor" . DIRECT
 #[\PHPUnit\Framework\Attributes\RequiresPhpExtension('pdo_pgsql')]
 final class PostgreSQLTest extends \PHPUnit\Framework\TestCase
 {
-    protected static ?\aportela\DatabaseWrapper\DB $db;
+    protected static \aportela\DatabaseWrapper\DB $db;
 
     private static string $host;
     private static int $port;
